17f9361d004eb542155e4b631409ae600c7cd976
James Paliga <jamespaliga@hotmail.com>
2024-06-19 19:30:06 +0100

    Remove unused functions

--- src/data_management.py
+++ src/data_management.py
@@ -8,15 +8,5 @@ def load_patient_data_raw():
     df = pd.read_csv("outputs/datasets/collection/HeartDiseasePrediction.csv")
     return df
 
-@st.cache_data
-def load_cleaned_test_set():
-    df = pd.read_csv("outputs/datasets/cleaned/TestSetCleaned.csv")
-    return df
-
-@st.cache_data
-def load_cleaned_train_set():
-    df = pd.read_csv("outputs/datasets/cleaned/TrainSetCleaned.csv")
-    return df
-
 def load_pkl_file(file_path):
     return joblib.load(filename=file_path)
