85d8dbe47f0d6275e0ffd5a30b8f0d305df616d9
James Paliga <jamespaliga@hotmail.com>
2024-06-22 15:47:00 +0100

    Update confusion matrix functions to write to streamlit instead of printing to terminal

--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -7,21 +7,19 @@ def confusion_matrix_and_report(X, y, pipeline, label_map):
 
     prediction = pipeline.predict(X)
 
-    print('---  Confusion Matrix  ---')
-    print(pd.DataFrame(confusion_matrix(y_true=prediction, y_pred=y),
+    st.write('---  Confusion Matrix  ---')
+    st.code(pd.DataFrame(confusion_matrix(y_true=prediction, y_pred=y),
           columns=[["Actual " + sub for sub in label_map]],
           index=[["Prediction " + sub for sub in label_map]]
           ))
-    print("\n")
 
-    print('---  Classification Report  ---')
-    print(classification_report(y, prediction, target_names=label_map), "\n")
+    st.write('---  Classification Report  ---')
+    st.code(classification_report(y, prediction, target_names=label_map), "\n")
 
 
 def clf_performance(X_train, y_train, X_test, y_test, pipeline, label_map):
-    print("#### Train Set #### \n")
+    st.warning("#### Train Set #### \n")
     confusion_matrix_and_report(X_train, y_train, pipeline, label_map)
 
-    print("#### Test Set ####\n")
+    st.warning("#### Test Set ####\n")
     confusion_matrix_and_report(X_test, y_test, pipeline, label_map)
-    
